6f419034113281a74cd51f6dcf3e4dd7cba90762
hoverzhao <gaspainton@126.com>
2025-07-21 08:34:37 +0800

    vscode

--- config/skeleton.c
+++ config/skeleton.c
@@ -1,8 +1,5 @@
 #include "bits/stdc++.h"
-using namespace std;
-
-typedef long long ll;
-mt19937 rnd((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
+#include "bits/competitive.h"
 
 static int x = []() {
 	ios::sync_with_stdio(false); cin.tie(0);
